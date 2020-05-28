c4c54d9f8f83a8273e96df91ca9fe78111e32209
Wesley <wesleybruno.so@gmail.com>
2020-05-28 16:53:41 -0300

    add path provider, remove interfacelocal storage

--- lib/app/data/sqflite.dart
+++ lib/app/data/sqflite.dart
@@ -1,26 +1,26 @@
-import '../interfaces/local_storage_interface.dart';
+class SqFlite {
 
-class SqFlite implements LocalStorageInterface {
-  @override
   Future read(String key) {
     // TODO: implement read
     throw UnimplementedError();
   }
 
-  @override
+
   void remove(String key) {
     // TODO: implement remove
   }
 
-  @override
+
   void save(keyue) {
     // TODO: implement save
   }
 
-  @override
+
   void update(String key, value) {
     // TODO: implement update
   }
 
-  Future readAll() {}
+  Future readAll() {
+    // TODO: implement update
+  }
 }
--- lib/app/interfaces/local_storage_interface.dart
+++
@@ -1,6 +0,0 @@
-abstract class LocalStorageInterface {
-  Future read(String key);
-  void save(dynamic value);
-  void update(String key, dynamic value);
-  void remove(String key);
-}
